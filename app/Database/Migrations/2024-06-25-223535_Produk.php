83f31a964c39de10d3b405aa60232e7cfe2bd651
rudi <rudi.pratm@gmail.com>
2024-06-29 23:11:00 +0800

    + migration produk

--- app/Database/Migrations/2024-06-25-223535_Produk.php
+++ app/Database/Migrations/2024-06-25-223535_Produk.php
@@ -8,11 +8,45 @@ class Produk extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_produk' => [
+                'type'           => 'INT',
+                'auto_increment' => true
+            ],
+            'slug_produk' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'kategori_slug' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'nama_produk' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'deskripsi' => [
+                'type'       => 'TEXT',
+            ],
+            'gambar_produk' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'tanggal_input' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'tanggal_ubah' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id_produk', true);
+        $this->forge->createTable('produk');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('produk');
     }
 }
